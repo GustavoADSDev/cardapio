4856c9f76e79102e08a4257392ae370b7af640c8
GustavoADSDev <eogustavinho6@gmail.com>
2025-04-09 15:24:30 -0300

    terminei

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ menu.addEventListener("click", function(event){
     let parentButton  = event.target.closest(".add-to-card-btn")
     if(parentButton){
         const name = parentButton.getAttribute("data-name")
-        const price = parseFloat (parentButton.getAttribute("data-price"))
+        const price = parseFloat(parentButton.getAttribute("data-price"))
 
         addToCart(name, price)
 //FIM
@@ -79,22 +79,136 @@ function updateCartModal(){
     let total = 0;
 
     cart.forEach(item => {
-        const cartItemElement = document.createElement ("div")
+        const cartItemElement = document.createElement ("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
 
         cartItemElement.innerHTML = `
-    <div>
+
+    <div class="flex items-center justify-between">
         <div>
-            <p>${item.name}</p>
-            <p>${item.quantity}</p>
-            <p>${item.price}</p>
+            <p class="font-bold">${item.name}</p>
+            <p>Qtd: ${item.quantity}</p>
+            <p class= "font-medium mt-2">R$ ${item.price.toFixed(2) }</p>
         </div>
 
-        <div>
-        <button> Remover </button>
+        <div class="remove-from-cart-btn" data-name= "${item.name}">
+       Remover 
         </div>
 
     </div>
-     `
+     ` 
+
+     total += item.price * item.quantity;
+
      cartItemsContainer.appendChild(cartItemElement)
     })
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+       style: "currency",
+       currency: "BRL" 
+    });
+
+    cartCounter.innerHTML = cart.length;
+
+}
+
+//FUNCAO PARA REMOVER ITEM DO CARRINHO
+cartItemsContainer.addEventListener("click", function (event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removerItemCart(name);
+    }
+})
+function removerItemCart(name){
+    const index = cart.findIndex (item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity  > 1){
+            item.quantity -=1
+            updateCartModal();
+            return
+        }
+        cart.splice(index, 1)
+        updateCartModal();
+    }
+
+}
+
+addressInput.addEventListener("input", function(event){
+  let inputValue = event.target.value;
+
+  if(inputValue !== "")
+    addressInput.classList.remove("border-red-500")
+    addressWarn.classList.add("hidden")
+})
+//FINALIZAR PEDIDO
+checkoutBtn.addEventListener("click", function(){
+   
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        
+       
+        Toastify({
+            text: "RESTAURANTE ESTÁ FECHADO",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#ef4444",
+            },
+            }).showToast();
+
+
+        return;
+    }
+    
+    if(cart.length === 0 ) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+        
+    }
+
+      //EVIAR API PARA WHATS
+      const cartItems = cart.map((item)=>{
+        return (
+            `${item.name} Quantidade:  (${item.quantity}) Preço: R$${item.price} | `
+        )
+        
+      }).join("")
+
+      const message = encodeURIComponent(cartItems)
+      const phone = "19991616111"
+
+      window.open(`https://wa.me/${phone}?text=${message} Enderenço: ${addressInput.value}`, "_black")
+
+      cart = [];
+      updateCartModal();
+})
+
+
+//VERIFICAR A HORA E MANIPULAR O CARD HORARIO
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora <22; 
+    //TRUE = RESTAURANTE ABERTO
+}
+
+
+const spanItem = document.getElementById ("date-span")
+const isOpen = checkRestaurantOpen();
+
+if (isOpen){
+spanItem.classList.remove("bg-red-500");
+spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
 }
